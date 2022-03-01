83d37b72faf65d701a22abc2ad32a8748b76fdb3
Mandeep Singhmar <msingh8328@gmail.com>
2022-03-01 15:27:56 +0530

    refactor

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -12,7 +12,7 @@ export { default as Searchbar } from './Searchbar';
 export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
 export { default as Stacked } from './Charts/Stacked';
-export { default as Doughnut } from './Charts/Doughnut';
+export { default as Pie } from './Charts/Pie';
 export { default as ChartsHeader } from './ChartsHeader';
 export { default as Header } from './Header';
 
